14c0ef8cd6ecee3447a0a707222cf518cc66ed2d
katakamadbutha <katakamadbutha9@gmail.com>
2026-08-14 14:22:41 +0530

    files added

--- frontend/index.js
+++ frontend/index.js
@@ -1,27 +1,55 @@
 // alert("hello from js")
 const users = [
- {
-   "name" : "Jonn Doe",
-   "gender" : "Male",
-   "image" : "john.png"
- },
-   {
-   "name" : "Jane Doe",
-   "gender" : "Female",
-   "image" : "jane.png"
- }
-]
+    {
+        name: "John Doe",
+        gender: "Male",
+        image: "john.png"
+    },
+    {
+        name: "Jane Doe",
+        gender: "Female",
+        image: "jane.png"
+    }
+];
 
-var curIndex = 0;
+let curIndex = 0;
 
-function toggle(){
- if(curIndex == 0)
-   curIndex = 1;
- else
-   curIndex = 0;
+function toggle() {
 
- document.getElementById("card-image").src = users[curIndex].image
- document.getElementById("card-name").innerText =users[curIndex].name
- document.getElementById("card-gender").innerText =users[curIndex].gender
- //gender needs to be updated
+    if (curIndex === 0) {
+        curIndex = 1;
+    } else {
+        curIndex = 0;
+    }
+
+    document.getElementById("user-card-image").src = users[curIndex].image;
+    document.getElementById("user-card-name").innerText = users[curIndex].name;
+    document.getElementById("user-card-gender").innerText = users[curIndex].gender;
+}
+
+
+function random() {
+
+    fetch("https://randomuser.me/api/")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+
+            const detail = data.results[0];
+
+            document.getElementById("user-card-image").src = detail.picture.large;
+
+            document.getElementById("user-card-gender").innerText = detail.gender;
+
+            const fullname =
+                detail.name.title + " " +
+                detail.name.first + " " +
+                detail.name.last;
+
+            document.getElementById("user-card-name").innerText = fullname;
+        })
+        .catch(function(error) {
+            console.log("Error:", error);
+        });
 }
