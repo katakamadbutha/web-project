74b36c8fc7472d506d3eebe102b44a0794c471bf
katakamadbutha <katakamadbutha9@gmail.com>
2026-08-14 18:25:36 +0530

    updated server.js file

--- server.js
+++ server.js
@@ -1,10 +1,134 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const app = express()
 
-const port = 8081;
-app.use(express.static("frontend"))
+app.use(express.json())
 
-app.listen(port, function () {
-    console.log(`My app is running at http://localhost:${port}`);
+const port = process.env.PORT || 8080
+
+const users = [
+ {
+   "id" : 1,
+   "name" : "Alejandra Romero",
+   "gender" : "male",
+   "image" : "https://randomuser.me/api/portraits/women/62.jpg"
+ },
+ {
+   "id" : 2,
+   "name" : "Olivia Morris",
+   "gender" : "female",
+   "image" : "https://randomuser.me/api/portraits/women/31.jpg"
+ },
+ {
+        "id":"2",
+        "name":"amber",
+        "gender":"female",
+        "image":"https://randomuser.me/api/portraits/women/43.jpg",
+    },
+
+    {
+        "id":"3",
+        "name":"lily",
+        "gender":"female",
+        "image":"https://randomuser.me/api/portraits/women/26.jpg",
+    },
+
+    {
+        "id":"4",
+        "name":"juan",
+        "gender":"male",
+        "image":"https://randomuser.me/api/portraits/men/88.jpg",
+    },
+
+    {        "id":"5",
+        "name":"valtteri rantala",
+        "gender":"male",
+        "image":"https://randomuser.me/api/portraits/men/5.jpg",
+    }
+]
+
+// api server
+
+// get all users
+app.get("/api/users", function(req, res){
+ res.status(200).json(users);
+})
+
+function getUserById(uid){
+ for(var i=0; i<users.length; i++)
+ {
+   if(uid == users[i].id)
+     return i;
+ }
+ return -1;
+}
+
+// get user by id
+app.get("/api/users/:id", function(req, res)
+{
+ var uid = req.params.id;
+ var userid = getUserById(uid);
+
+ if(userid == -1)
+ {
+   res.status(404).json({"message" : "user not found"})
+ }
+ res.status(200).json(users[userid])
+})
+
+// get random user
+app.get("/api/randomuser", function(req, res){
+ var n = users.length;
+ const randomid = Math.floor(Math.random() * n);
+ res.status(200).json(users[randomid])
+})
+
+var newuserid = users.length + 1;
+
+// post: add a new user
+app.post("/api/users", function(req, res)
+{
+ if(!req.body.name || !req.body.gender || !req.body.image)
+   return res.json({"message" : " name, gender and image is required"})
+ let user = req.body;
+ user.id = newuserid;
+ newuserid++;
+ users.push(user);
+ res.status(200).json({"message" : "added successfully","user":user});
+});s
+
+// put: update user details of given id
+app.put("/api/users/:id", function(req, res){
+ var userid = getUserById(req.params.id);
+
+ if(userid == -1)
+   return res.json({"message" : "user not found"});
+
+   if(req.body.name)
+     users[userid].name = req.body.name;
+
+   if(req.body.gender)
+     users[userid].gender = req.body.gender;
+
+   if(req.body.image)
+     users[userid].image = req.body.image;
+
+   return res.status(200).json({"message" : "user details updated", "user" : users[userid]});
 });
+
+app.delete("/api/users/:id", function(req, res){
+ var userid = getUserById(req.params.id);
+ if(userid == -1)
+   return res.json({"message" : "user not found"});
+
+ users.splice(userid, 1);
+
+ res.status(200).json({"message" : "user deleted successfully"});
+
+});
+
+app.use(express.static("frontend")) // web server
+
+app.listen(port, function (){
+ console.log("my app is running at http://localhost:"+port)
+})
